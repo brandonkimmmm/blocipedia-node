ee0babab0b05d4464cac272d423af91bb90ec4dc
Brandon Kim <bkim2490@gmail.com>
2018-10-17 23:47:15 +0900

    scaffolded and set up for deployment

---
+++ src/config/main-config.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const path = require('path');
+const viewsFolder = path.join(__dirname, '..', 'views');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const session = require('express-session');
+const flash = require('express-flash');
+const logger = require('morgan');
+
+module.exports = {
+    init(app, express){
+        app.set('views', viewsFolder);
+        app.set('view engine', 'ejs');
+        app.use(logger('dev'));
+        app.use(express.static(path.join(__dirname, '..', 'assets')));
+    }
+};
